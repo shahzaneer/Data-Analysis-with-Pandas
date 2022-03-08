25f11d5353845e515105110303b610a00677294d
Shahzaneer Ahmed <shahism01@gmail.com>
2022-03-08 20:42:22 +0500

    read,write in cvs added

---
+++ LimitedNewdata.csv
@@ -0,0 +1,5 @@
+,Personal Evaluation  Test Fall 21,Unnamed: 1,Unnamed: 2,Unnamed: 3,Unnamed: 4,Unnamed: 5
+0,Timestamp,name,relation,good,bad,suggestions
+1,2/10/2021 17:12,Shah,Others,You're hardworking,You're an overthinker,Keep calm 
+2,2/10/2021 17:25,Sameer Doultana,University friend,"Helpful, Cooperative, well-mannered (& many more)","Less social, less friendly",Be friendly and don't take everything serious and offensive
+3,2/10/2021 17:25,Adam ke ullad,Others,Tu muze hai,Charse jahaz,Chal puttur tu chuti kr
---
+++ someColumn.cpp
@@ -0,0 +1,74 @@
+,name,good
+0,Shah,You're hardworking
+1,Sameer Doultana,"Helpful, Cooperative, well-mannered (& many more)"
+2,Adam ke ullad,Tu muze hai
+3,Ramsha Shakeel,Active and passionate
+4,Usama Ejaz,Friendly and Helpful
+5,,Friendly behaviour
+6,,Straightforwardness. 
+7,Sameem amjad,A nice and straight forward person with a little bit complexity😃
+8,Ali haider,Hardworker
+9,M.Ahmad Junaid,nothing😁
+10,Ali,"You are hardworking, devoted towards whatever you like to do...you are proud of your accomplishments and like to express your feelings..."
+11,Shazil,Your consistency with your work
+12,Zohaib Mallk,Your behaviour
+13,Mohid Anwar,Bawa Jee your personality.
+14,Hassam Talha,You are nice and humble.
+15,Arslan Shahid,Intelligence
+16,Muhammad Ali,A good friend 
+17,Waqas Ahmed ,Good Person
+18,Ahmad Raza,I think ...
+19,Huzaifa Adeel,I found you very nice and helpful as person.
+20,,Cooperative
+21,,Nature
+22,Sayyam,A dedicated person
+23,Hafiz Amir Ijaz,Nice Personality 
+24,Abdullah zaib,Highly talented and coperative
+25,,Fuck u 😅
+26,Usama Zahid,Nice Content Writer and a responsible person
+27,Muhammad Azeem,"You are kind, and helping person."
+28,,Helping
+29,Sohaib Shams,The ability to survive in struggling years
+30,Salman shahif hameed,Sincere with your work
+31,Ab Warsi,Beautiful boy
+32,ShahBano,The way you keep yourself motivated
+33,,Honesty 
+34,Rana Mansoor,Consistency 
+35,Syed Musab Pirzada,One of those people who love to help and guide others.
+36,Omar,Don't know you very well but you seem pretty positive and honest.
+37,Azan Ali ,Intelligent 
+38,Asher,You being straight forward 
+39,Talha Waheed ,You are sincere and straight forward 
+40,Muhammad Faisal Shahzad Nadeem,Good way of taking to others
+41,Taimoor Hasan,Innocence
+42,Manahil,Technical minded which is good for you to proceed in competitive world. Intellectual person and strong potential to deal things profoundly
+43,Rahim,You are intelligent and serious about your future that's a good thing.... And your communications skills are also good
+44,Alveena Tanis,Extraordinary talented
+45,,"Friendly ,hard working"
+46,Saim,Till now nothing
+47,Sahil acharya,Alot things 
+48,pawraj singh,good looking and better character than others
+49,,Intelligence 
+50,Usman ,Laiq ho
+51,Ubaid,Character
+52,Abdullah,Your hardwork and dedication
+53,,Knowledge
+54,Sartaj Mehrani,Everything
+55,Abdullah,Nature
+56,,"Enthusiastic, wants to improve, learn and grow in life"
+57,teri Maa😂🤓😎,Nothing bruhh🤓😂
+58,Umair Ahmad,Spirit for learning
+59,Muhammad Hazrat,Enthusiam
+60,QAMAR ZAMAN ,King hearted and i see a soft hearted inside u 
+61,Zuwanish,Cooperative
+62,Ahmad,Ap bolta bat kam ho😅😅
+63,,Helping and a good fellow.
+64,Fatima,"The good thing is you are friendly, and you share your future goals with other people and ask for suggestions."
+65,Yahya,Courage to do productive things without hesitation.
+66,Rohail Ahmed,"Confidence in asking questions
+You give respect
+Little bit different
+Css aspirant"
+67,DH,A responsible guy having clear aims for his life. 
+68,Mohsin Satti,Your humbleness 
+69,Mahad,Nothing 
